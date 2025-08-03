e684aa2079a56c945f1f8f8096ed1fd4270de138
Emma Tsang <20544166@student.westernsydney.edu.au>
2025-08-03 19:14:46 +1000

    added address debugging print statement

--- tests/message_recv_test.py
+++ tests/message_recv_test.py
@@ -4,6 +4,7 @@ PORT_NUMBER = 50514
 
 def main():
     recv = Receiver(port=PORT_NUMBER)
+    print(f"RECEIVER CREATED @ {recv.addr}")
     while True:
         message,addr = recv.listen()
         print(f"message {message} recv from {addr}")
